7fa10ff8e91bd7c3790ff16aa3187d403d009e88
Maxim Myasnenko <MaxChip322@yandex.ru>
2025-05-22 23:43:49 +0700

    Complete some binary tree problems

---
+++ Maximum Depth of Binary Tree/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Maximum_Depth_of_Binary_Tree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+  Definition for a binary tree node.
+  public class TreeNode {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
+        public class Solution
+        {
+            public int MaxDepth(TreeNode root)
+            {
+
+            }
+        }
+    }
+}
---
+++ Validate Binary Search Tree/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Validate_Binary_Search_Tree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(10 < null);
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public class Solution
+        {
+            //int lv = root.left == null ? root.left.val : null;
+            //int rv = root.right == null ? root.val : root.right.val;
+            public bool IsValidBST(TreeNode root)
+            {
+                if (root == null) return true;
+
+                if (root.left == null && root.right == null) return true;
+                else if (root.left == null) return root.val < root.right.val;
+                else if (root.right == null) return root.val > root.left.val;
+
+                if (root.val <= root.left.val || root.val >= root.right.val) return false;
+
+                return IsValidBST(root.left) && IsValidBST(root.right);
+            }
+        }
+    }
+}
